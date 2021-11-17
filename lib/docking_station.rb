78b4dc4c5e6e720e82fcbe208d55dcb3b7c8a853
riky moroni <riky5.moroni@gmail.com>
2021-11-17 16:46:17 +0000

    add dock and bike_available method and tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,21 @@
 class DockingStation
+
+  attr_reader :docked_bikes
+
+  def initialize
+    @docked_bikes = []
+  end
+
   def release_bike(bike)
     bike
   end
+
+  def dock(bike)
+    docked_bikes << bike
+  end
+
+  def bike_available
+    docked_bikes[-1]
+  end
+
 end
--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -9,13 +9,29 @@ describe DockingStation do
     docking_station = DockingStation.new
     expect(docking_station.release_bike(bike)).to be_instance_of(Bike)
   end
+
   it "Expects the bike to be working" do
     bike = Bike.new(bike)
     expect(bike.working?).to eq true
   end
+
   it "Should get true when call the method working on DockingStation.release_bike" do
     docking_station = DockingStation.new
     bike = Bike.new(bike)
     expect(docking_station.release_bike(bike).working?).to eq true
   end
+
+  it "should be able to dock a bike" do
+    docking_station = DockingStation.new
+    bike = Bike.new(bike)
+    expect(docking_station.dock(bike)).to include bike
+  end
+
+  it "should be able to see if a bike has been docked" do
+    docking_station = DockingStation.new
+    bike = Bike.new(bike)
+    docking_station.dock(bike)
+    expect(docking_station.bike_available).to eq bike
+  end
+
 end
